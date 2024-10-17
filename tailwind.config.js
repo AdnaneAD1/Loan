1f285d3a2ab2e7aef4ee1b11ea86f3d437481055
AdnaneAD1 <135954913+AdnaneAD1@users.noreply.github.com>
2024-10-17 21:28:24 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    safelist: [
+        'clearfix',
+        'dropdown',
+        'navigation',
+        // Ajoutez toutes les autres classes que vous souhaitez protéger ici
+    ],
     theme: {},
     plugins: [require('@tailwindcss/forms')],
 }
